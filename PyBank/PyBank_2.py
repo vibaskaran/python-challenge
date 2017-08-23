172e0716ea506df73d6a2b9be1b798396d5ba773
Baskaran Viswanathan <vibaskaran@live.com>
2017-08-22 23:24:48 -0400

    Corrected

--- PyBank/PyBank_2.py
+++ PyBank/PyBank_2.py
@@ -1,59 +1,90 @@
-import os
+# Dependencies
 import csv
 import locale
-
-
-csvpath = os.path.join("raw_data", "budget_data_2.csv")
-
-output_path = os.path.join('output', 'budget_data_2_output.csv')
 locale.setlocale( locale.LC_ALL, '' )
-csv_dict_with_strs = {}
-csv_dict_with_ints = {}
-Total_Months = 0
-Total_Revenue = 0
-Average_Revenue = 0
 
-with open(csvpath, newline="") as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
-    next(csvreader, None)
-    csv_dict_with_strs = dict(csvreader)
+# Files to Load
+file_to_load = "raw_data/budget_data_2.csv"
+file_to_output = "output/budget_analysis_2.txt"
 
-csv_dict_with_ints = dict((k,int(v)) for k,v in csv_dict_with_strs.items()) # Converting the Value column as integer
+# Variables to Track
+total_months = 0
+total_revenue = 0
 
-Total_Months = len(csv_dict_with_ints.keys()) # getting total no of keys to find out total months 
+prev_revenue = 0
+revenue_change = 0
+greatest_increase = ["", 0]
+greatest_decrease = ["", 9999999999999999999999]
 
-Total_Revenue = sum(csv_dict_with_ints.values()) # getting total or all the values to find out total Revenue
+revenue_changes = []
 
-Average_Revenue = Total_Revenue / Total_Months # getting Average Revenue
+# Read Files
+with open(file_to_load) as revenue_data:
+    reader = csv.DictReader(revenue_data)
 
-min_value = min(csv_dict_with_ints.values())  # minimum value
-min_key = [k for k, v in csv_dict_with_ints.items() if v == min_value] # getting all keys containing the `minimum`
+    # Loop through all the rows of data we collect
+    for row in reader:
 
-max_value = max(csv_dict_with_ints.values())  # maximum value
-max_key = [k for k, v in csv_dict_with_ints.items() if v == max_value] # getting all keys containing the `maximum`
+        # Calculate the totals
+        total_months = total_months + 1
+        total_revenue = total_revenue + int(row["Revenue"])
+        # print(row)
 
-print()
-with open(output_path, 'w', newline='') as csvfile:
+        # Keep track of changes
+        revenue_change = int(row["Revenue"]) - prev_revenue
+        # print(revenue_change)
+
+        # Reset the value of prev_revenue to the row I completed my analysis
+        prev_revenue = int(row["Revenue"])
+        # print(prev_revenue)
+
+        # Determine the greatest increase
+        if (revenue_change > greatest_increase[1]):
+            greatest_increase[1] = revenue_change
+            greatest_increase[0] = row["Date"]
+
+        if (revenue_change < greatest_decrease[1]):
+            greatest_decrease[1] = revenue_change
+            greatest_decrease[0] = row["Date"]
+
+        # Add to the revenue_changes list
+        revenue_changes.append(int(row["Revenue"]))
+
+    # Set the Revenue average
+    revenue_avg = sum(revenue_changes) / len(revenue_changes)
     
-    print('Financial Analysis')
-    csvfile.writelines(['Financial Analysis'+ '\n'])
+    # Show Output
+    print("Total Months: " + str(total_months))
+    print("Total Revenue: " + locale.currency(total_revenue,grouping = True))
+    print("Average Revenue Change: " + locale.currency(revenue_avg,grouping = True))
+    print("Greatest Increase: " + str(greatest_increase[0]) + '  ('+ locale.currency(greatest_increase[1],grouping = True)+')')
+    print("Greatest Decrease: " + str(greatest_decrease[0])  + '  ('+ locale.currency(greatest_decrease[1],grouping = True)+')')
+    
 
-    print('----------------------------')
-    csvfile.writelines(['----------------------------'+ '\n'])
+# # Generate Output Summary
+# output = (
+#     f"\nFinancial Analysis\n"
+#     f"----------------------------\n"
+#     f"Total Months: {total_months}\n"
+#     f"Total Revenue: ${total_revenue}\n"
+#     f"Average Revenue Change: ${revenue_avg}\n"
+#     f"Greatest Increase in Revenue: {greatest_increase[0]} (${greatest_increase[1]})\n"
+#     f"Greatest Decrease in Revenue: {greatest_decrease[0]} (${greatest_decrease[1]})\n")
 
+# Print the output (to terminal)
+# print(output)
 
-    print('Total Months: ', Total_Months )
-    #csvfile.writelines(['Total Months:  ', len(csv_dict_with_ints.keys()) + '\n'])
-
-    print('Total Revenue: ',locale.currency(Total_Revenue,grouping = True))
-    #csvfile.writelines(['Total Revenue: ',locale.currency(Total_Revenue,grouping = True)+ '\n'])
-
-    print('Average Revenue Change: ',locale.currency(Average_Revenue,grouping = True))
-    #csvfile.writelines(['Average Revenue Change: ',locale.currency(Average_Revenue,grouping = True)+ '\n'])
-
-    print('Greatest Increase in Revenue: ',max_key,locale.currency(max_value,grouping = True) )
-    #csvfile.writelines(['Greatest Increase in Revenue: ',max_key,locale.currency(max_value,grouping = True)+'\n'])
-
-    print('Greatest Decrease in Revenue: ',min_key,locale.currency(min_value,grouping = True) )
-    #csvfile.writelines(['Greatest Decrease in Revenue: ',min_key,locale.currency(min_value,grouping = True)])
-print()
+# Output Files
+with open(file_to_output, "w") as txt_file:
+    txt_file.write("Total Months: " + str(total_months))
+    txt_file.write("\n")
+    #locale.currency(Average_Revenue,grouping = True)
+    txt_file.write("Total Revenue: " + locale.currency(total_revenue,grouping = True))
+    txt_file.write("\n")
+    txt_file.write("Average Revenue Change: " + locale.currency(revenue_avg,grouping = True))
+    txt_file.write("\n")
+    txt_file.write("Greatest Increase: " + str(greatest_increase[0]) + '  ('+ locale.currency(greatest_increase[1],grouping = True)+')')
+    txt_file.write("\n")
+    txt_file.write("Greatest Decrease: " + str(greatest_decrease[0])  + '  ('+ locale.currency(greatest_decrease[1],grouping = True)+')')
+    txt_file.write("\n")
+    
